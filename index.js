925d86ddb75e3d34453a7ace6d36680b91ccaeb5
Ryan Christian <ryanchristian4427@gmail.com>
2021-04-28 16:28:15 -0500

    refactor: Replacing babel-eslint with @babel/eslint-parser, as babel-eslint has been deprecated.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
 	// TODO: this is really only required for class property initializer methods, which are seeing declining usage.
 	// At some point, we should un-ship the custom parser and let ESLint use esprima.
-	parser: require.resolve('babel-eslint'),
+	parser: require.resolve('@babel/eslint-parser'),
 
 	// Currently ignored due to the custom parser.
 	parserOptions: {
@@ -13,6 +13,10 @@ export default {
 			experimentalObjectRestSpread: true,
 			experimentalDecorators: true,
 			jsx: true
+		},
+		requireConfigFile: false,
+		babelOptions: {
+			plugins: ['@babel/plugin-syntax-jsx', '@babel/plugin-syntax-class-properties']
 		}
 	},
 
